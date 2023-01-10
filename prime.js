2f59ab4a4520ac7165ad76afa767377f17d8437b
Tarun Ergurala <tarunergurala7030@gmail.com>
2023-01-10 22:12:26 +0530

    uploaded

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num = 13;
+let count = 0;
+
+for(let i = 0 ; i<=num; i--){
+    if(num%i==0){
+        count++;
+    }
+}
+if(count==2)[
+    console.log("is prime");
+]else{
+    console.log("is not a prime");
+}
